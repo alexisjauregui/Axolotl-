3ed20c0888644a2f81f22b8dc4cfd71d8d511d50
ALEXIS-DESKTOP\AlexisJ <ajaur93@gmail.com>
2017-07-30 11:43:02 -0700

    Added Test Script

---
+++ Axolotl/Assets/Scripts/test_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_script : MonoBehaviour {
+    // BEeep beep whos got the key to the jeep
+    // Vroooooooooooom
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
